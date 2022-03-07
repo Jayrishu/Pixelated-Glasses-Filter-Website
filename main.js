a935ffd920642d9149e6feb39a66631edee8211f
Jayrishu <91966368+Jayrishu@users.noreply.github.com>
2022-03-07 18:43:30 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload(){
+}
+function setup(){
+    canvas = createCanvas(500,600);
+    canvas.center();
+}
+function draw(){
+
+}
+function take_photo(){
+    save("Pixelated_Glasses_Filter.png");
+}
